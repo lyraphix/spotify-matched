3b480cd75b6d49d87c3123112422a858654e5b17
Vanessa Wei <ywei84@emory.edu>
2023-02-07 14:50:51 -0500

    debug

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Container, CssBaseline, Typography, Box, Grid, TextField, FormControlLabel, Checkbox, ThemeProvider, Button } from "@mui/material/";
+import { Appbar, Toolbar, Container, CssBaseline, Typography, Box, Grid, TextField, FormControlLabel, Checkbox, ThemeProvider, Button } from "@mui/material/";
 
 
 // import { Formik, field, form} from 'formik'
@@ -17,8 +17,7 @@ export default function Home() {
   return (
     <Container>
       <CssBaseline />   
-      {/* <Grid Container spacing = {2}> */}
-      <Box
+      {/* <Box
             sx={{
               margin: 8,
               display: 'flex',
@@ -26,13 +25,13 @@ export default function Home() {
               alignItems: 'center',
               
               // backgroundColor: 'primary.light',
-            }}>
+            }}> */}
           {/* <Grid item xs={2}> */}
-            <Box sx={{
+            {/* <Box sx={{
               margin: 10,
               display: 'flex',
               flexDirection: 'row',
-            }}>
+            }}> */}
               
               <Typography variant='h1'>
                 Spotify
@@ -41,13 +40,9 @@ export default function Home() {
                 MATCHED
               </Typography>
               <></>
-            </Box>
+            {/* </Box> */}
 
-        <Box item xs={12} sm={6} sx={
-          {alignItems: 'center',
-          display: 'flex',
-          flexDirection: 'row',}
-        }>
+        
         <Button
             variant="contained"
             size = "medium"
@@ -56,12 +51,21 @@ export default function Home() {
         >
             Sign Up
         </Button>
-        </Box>
+        
+        <Box
+             sx={{
+               margin: 8,
+               display: 'flex',
+               flexDirection: 'column',
+               alignItems: 'center',
+
+               // backgroundColor: 'primary.light',
+             }}>
         <div>
           SPOTIFY MAAAAATCHED!!!
         </div>
-        
-      </Box>
+        </Box>
+      {/* </Box> */}
       {/* </Grid> */}
     </Container>
   )
