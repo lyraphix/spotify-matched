229d53d64700234c3c4592835eb3923ec8010d96
Vanessa Wei <ywei84@emory.edu>
2023-02-07 14:47:44 -0500

    debug

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,56 +16,53 @@ export default function Home() {
   }
   return (
     <Container>
-       <CssBaseline />   
-            <Box >
-               <AppBar position="static">
-                 <Toolbar>
-                   {/* <Box sx={
-                       {alignItems: 'left',
-                       display: 'flex',
-                       flexDirection: 'row'}
-                     }> */}
+      <CssBaseline />   
+      {/* <Grid Container spacing = {2}> */}
+      <Box
+            sx={{
+              margin: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              
+              // backgroundColor: 'primary.light',
+            }}>
+          {/* <Grid item xs={2}> */}
+            <Box sx={{
+              margin: 10,
+              display: 'flex',
+              flexDirection: 'row',
+            }}>
+              
+              <Typography variant='h1'>
+                Spotify
+              </Typography>
+              <Typography variant='h2'>
+                MATCHED
+              </Typography>
+              <></>
+            </Box>
 
-                         <Typography variant='h1' sx={{ ml: 2}}>
-                           Spotify
-                         </Typography>
-                         <Typography variant='h2' sx={{ flexGrow: 11 }}>
-                           MATCHED
-                         </Typography>
-                   {/* </Box> */}
-                         <Button
-                               variant="text"
-                               size = "medium"
-                               sx={{ flexGrow: 1 }}
-                               href="/posts/first-post"
-                           >
-                               Login
-                         </Button>
-                         <Button
-                               variant="contained"
-                               size = "medium"
-                               href="/posts/first-post"
-                           >
-                               Sign Up
-                         </Button>
-
-                 </Toolbar>
-               </AppBar>
-         </Box>
-         <Box
-             sx={{
-               margin: 8,
-               display: 'flex',
-               flexDirection: 'column',
-               alignItems: 'center',
-
-               // backgroundColor: 'primary.light',
-             }}>
-         <div>
-           SPOTIFY MAAAAATCHED!!!
-         </div>
-
-       </Box>
-       </Container>
+        <Box item xs={12} sm={6} sx={
+          {alignItems: 'center',
+          display: 'flex',
+          flexDirection: 'row',}
+        }>
+        <Button
+            variant="contained"
+            size = "medium"
+            sx={{ mt: 15, mb: 10 }}
+            href="/posts/first-post"
+        >
+            Sign Up
+        </Button>
+        </Box>
+        <div>
+          SPOTIFY MAAAAATCHED!!!
+        </div>
+        
+      </Box>
+      {/* </Grid> */}
+    </Container>
   )
 }
