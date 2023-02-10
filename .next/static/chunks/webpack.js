1b0d6ad34ef4dd188e0b3ed700de4e5f05b1f564
Vanessa Wei <ywei84@emory.edu>
2023-02-09 22:25:09 -0500

    Adding box color

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "950046ec344d56f0"; }
+/******/ 		__webpack_require__.h = function() { return "1d299e753cca1c1a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
