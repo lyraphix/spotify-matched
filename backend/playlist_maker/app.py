24229dade2a12bf23e482bf6939b8142e530bee7
FredHino <fredhinojosa03@gmail.com>
2023-02-22 19:19:50 -0500

    added app.py and changed createPlaylist
    app.py handles auth stuff using flask. changed createPlaylist to take a generated access token and return the playlist link

---
+++ backend/playlist_maker/app.py
@@ -0,0 +1,79 @@
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from flask import Flask, url_for, session, request, redirect
+import json
+import time
+from createPlaylist import main
+
+
+# App config
+app = Flask(__name__)
+
+app.secret_key = 'SOMETHING-RANDOM'
+app.config['SESSION_COOKIE_NAME'] = 'spotify-login-session'
+
+@app.route('/')
+def login():
+    sp_oauth = create_spotify_oauth()
+    auth_url = sp_oauth.get_authorize_url()
+    print(auth_url)
+    return redirect(auth_url)
+
+@app.route('/authorize')
+def authorize():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get('code')
+    token_info = sp_oauth.get_access_token(code)
+    session["token_info"] = token_info
+    return redirect("/getTracks")
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect('/')
+
+@app.route('/getTracks')
+def get_all_tracks():
+    session['token_info'], authorized = get_token()
+    session.modified = True
+    if not authorized:
+        return redirect('/')
+
+    link = main(session.get('token_info').get('access_token'))
+
+    return link
+
+
+# Checks to see if token is valid and gets a new token if not
+def get_token():
+    token_valid = False
+    token_info = session.get("token_info", {})
+
+    # Checking if the session already has a token stored
+    if not (session.get('token_info', False)):
+        token_valid = False
+        return token_info, token_valid
+
+    # Checking if token has expired
+    now = int(time.time())
+    is_token_expired = session.get('token_info').get('expires_at') - now < 60
+
+    # Refreshing token if it has expired
+    if (is_token_expired):
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
+
+    token_valid = True
+    return token_info, token_valid
+
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id ='39951c143d3f4fd1b8a4159d349399e8',
+        client_secret='58ed9c7f0a7944efbc045a0393459e09',
+        redirect_uri=url_for('authorize', _external=True),
+        scope=['playlist-modify-private','playlist-modify-public','user-read-email','user-top-read','user-read-recently-played','user-read-private'])
+        
